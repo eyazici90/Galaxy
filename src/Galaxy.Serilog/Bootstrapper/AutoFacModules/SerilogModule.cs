c90a8a3b8c9209fa2d889d86a16bda9d03746327
eyazici <e.yazici1990@gmail.com>
2018-10-29 12:31:02 +0300

    serilog adds++

---
+++ src/Galaxy.Serilog/Bootstrapper/AutoFacModules/SerilogModule.cs
@@ -0,0 +1,20 @@
+﻿using Autofac;
+using Galaxy.Log;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Galaxy.Serilog.Bootstrapper.AutoFacModules
+{
+  
+    public class SerilogModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterType<GalaxySerilogLogger>()
+                 .As<ILog>()
+                 .SingleInstance();
+
+        }
+    }
+}
---
+++ src/Galaxy.Serilog/Bootstrapper/GalaxySerilogRegistrationExtensions.cs
@@ -0,0 +1,33 @@
+﻿using Autofac;
+using Galaxy.Log;
+using Galaxy.Serilog.Bootstrapper.AutoFacModules;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Galaxy.Serilog.Bootstrapper
+{
+    public  static class GalaxySerilogRegistrationExtensions
+    {
+        public static ContainerBuilder UseGalaxySerilogger(this ContainerBuilder builder)
+        {
+            builder.RegisterModule(new SerilogModule());
+            builder.RegisterModule(new SerilogDefaultConfigurationsModule());
+            return builder;
+        }
+        public static ContainerBuilder UseGalaxySerilogger<TConfiguration>(this ContainerBuilder builder, TConfiguration configurations )
+            where TConfiguration: ILogConfigurations
+        {
+            builder.RegisterModule(new SerilogModule());
+            InitializeConfigurations(builder, configurations);
+            return builder;
+        }
+        private static void InitializeConfigurations<TConfiguration>(this ContainerBuilder builder, TConfiguration configurations = default)
+            where TConfiguration : ILogConfigurations
+        {
+            builder.RegisterType(typeof(TConfiguration))
+                    .As<ILogConfigurations>()
+                    .SingleInstance();
+        }
+    }
+}
