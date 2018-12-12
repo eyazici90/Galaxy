f9fd2c292f93ce471b4f079459062675e4bb17ea
eyazici90 <e.yazici1990@gmail.com>
2018-12-12 15:35:11 +0300

    GalaxyElasticSearchRegistrationExtensions adds++

---
+++ src/Galaxy.ElasticSearch/Bootstrapper/GalaxyElasticSearchRegistrationExtensions.cs
@@ -0,0 +1,15 @@
+﻿using Autofac;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Galaxy.ElasticSearch.Bootstrapper
+{
+    public static  class GalaxyElasticSearchRegistrationExtensions
+    {
+        public static ContainerBuilder UseGalaxyElasticSearch(this ContainerBuilder builder)
+        { 
+            return builder;
+        }
+    }
+}
