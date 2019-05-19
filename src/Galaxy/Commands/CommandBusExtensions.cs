2430c897a73e71b06c686e0ef6666f9af7a3c88c
eyazici <e.yazici1990@gmail.com>
2019-05-19 13:17:35 +0200

    buses extensions adds++

---
+++ src/Galaxy/Commands/CommandBusExtensions.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Galaxy.Commands
+{
+    public static class CommandBusExtensions
+    {
+        public static void Send<TCommand>(this ICommandBus commandBus, TCommand commandData)
+         where TCommand : class
+        {
+            SendAsync(commandBus, commandData).ConfigureAwait(false)
+                .GetAwaiter().GetResult();
+        }
+
+        public static void Send(this ICommandBus commandBus, Type commandType, object commandData)
+        {
+            SendAsync(commandBus, commandData).ConfigureAwait(false)
+               .GetAwaiter().GetResult();
+        }
+
+        public static async Task SendAsync<TCommand>(this ICommandBus commandBus, TCommand commandData)
+            where TCommand : class
+        {
+            if (commandData == null) throw new ArgumentNullException(nameof(commandData));
+
+            await commandBus.SendAsync(commandData);
+        }
+
+        public static async Task SendAsync(this ICommandBus commandBus, Type commandType, object commandData)
+        {
+            if (commandData == null) throw new ArgumentNullException(nameof(commandData));
+
+            await commandBus.SendAsync(commandType, commandData);
+        }
+    }
+}
---
+++ src/Galaxy/Events/EventBusExtensions.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Galaxy.Events
+{
+    public static class EventBusExtensions
+    {
+        public static void Publish<TEvent>(this IEventBus eventBus, TEvent eventData)
+           where TEvent : class
+        {
+            PublishAsync(eventBus, eventData).ConfigureAwait(false)
+                .GetAwaiter().GetResult();
+        }
+
+        public static void Publish(this IEventBus eventBus, Type eventType, object eventData)
+        {
+            PublishAsync(eventBus, eventData).ConfigureAwait(false)
+               .GetAwaiter().GetResult();
+        }
+
+        public static async Task PublishAsync<TEvent>(this IEventBus eventBus, TEvent eventData)
+            where TEvent : class
+        {
+            if (eventData == null) throw new ArgumentNullException(nameof(eventData));
+
+            await eventBus.PublishAsync(eventData);
+        }
+
+        public static async Task PublishAsync(this IEventBus eventBus, Type eventType, object eventData)
+        {
+            if (eventData == null) throw new ArgumentNullException(nameof(eventData));
+
+            await eventBus.PublishAsync(eventType, eventData);
+        }
+    }
+}
